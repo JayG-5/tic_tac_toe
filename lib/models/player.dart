031c3e4fb2ffb7808bdbc7f2ce4e9118e8fb7824
JayG-5 <jg55552758@gmail.com>
2024-02-10 23:58:02 +0900

    feat: player model

---
+++ lib/models/player.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Player {
+  final Icon marker;
+  final Color color;
+  final int backsies;
+
+  Player({required this.marker, required this.color, required this.backsies});
+
+}
