63b137669ae75ee0111e366679539ac36b29f729
JayG-5 <jg55552758@gmail.com>
2024-02-11 15:57:10 +0900

    feat: Routes enum

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tic_tac_toe/enums/routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(),
+    return GetMaterialApp(
+      initialRoute: Routes.home.name,
+      getPages: Routes.pageBuilders,
     );
   }
 }
---
+++ lib/enums/routes.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tic_tac_toe/app.dart';
+
+enum Routes {
+  home('/', widget: MyApp());
+
+  final String name;
+  final Widget widget;
+  final Transition transition;
+  final Bindings? bindings;
+
+  const Routes(this.name,
+      {required this.widget,
+      this.transition = Transition.noTransition,
+      this.bindings});
+
+  GetPage get pageBuilder => GetPage(
+        name: name,
+        page: () => widget,
+        transition: transition,
+        binding: bindings,
+      );
+
+  static List<GetPage> get pageBuilders => Routes.values.map((e) => e.pageBuilder).toList();
+}
