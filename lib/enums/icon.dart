d78231abf1c2fef474ac4c30ed823ae328dfe158
JayG-5 <jg55552758@gmail.com>
2024-02-11 13:43:14 +0900

    feat: feature getNonUseIcons

--- lib/enums/icon.dart
+++ lib/enums/icon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/models/player.dart';
 
 enum PlayerIcon {
   cross(Icons.clear),
@@ -10,4 +11,13 @@ enum PlayerIcon {
   final IconData iconData;
 
   const PlayerIcon(this.iconData);
+
+  List<IconData> getNonUseIcons(List<Player> players) {
+    final playerIcons = players.map((e) => e.marker.icon?.codePoint).toList();
+    final nonUseIcons = PlayerIcon.values
+        .where((element) => !playerIcons.contains(element.iconData.codePoint))
+        .map((e) => e.iconData)
+        .toList();
+    return nonUseIcons;
+  }
 }
