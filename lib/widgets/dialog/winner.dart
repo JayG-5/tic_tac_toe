0717b7f5163c4e7156cbba584d1a52e96230243e
JayG-5 <jg55552758@gmail.com>
2024-02-12 17:05:36 +0900

    feat: winner dialog

---
+++ lib/widgets/dialog/winner.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:tic_tac_toe/models/game.dart';
+import 'package:tic_tac_toe/models/player.dart';
+
+class WinnerDialogView extends StatelessWidget {
+  const WinnerDialogView({super.key, required this.game, required this.player});
+
+  final Game game;
+  final Player player;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('승리!'),
+      content: Row(
+        children: [
+          const Text('승자: '),
+          Icon(
+            player.iconData,
+            color: player.color,
+          )
+        ],
+      ),
+      actions: [
+        ElevatedButton(onPressed: ()async{
+          final box = Hive.box('games');
+          await box.put(game.startTime.toIso8601String(), game.toJson());
+          Get.offAllNamed('/');
+        }, child: const Text('기록하고 홈으로')),
+        ElevatedButton(onPressed: ()=>Get.offAllNamed('/'), child: const Text('홈으로')),
+      ],
+    );
+  }
+}
