bea90f40115be01d2a577b104a56f1751a12b131
JayG-5 <jg55552758@gmail.com>
2024-02-12 19:40:51 +0900

    feat: LogDetailPageController

---
+++ lib/controllers/log_detail.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+import 'package:tic_tac_toe/models/game.dart';
+
+class LogDetailPageController extends GetxController{
+
+  LogDetailPageController(Game game){
+    this.game.value = game;
+  }
+  final Rx<Game?> game = Rx<Game?>(null);
+
+  static LogDetailPageController withGetX(){
+    final Game game = Get.arguments['game'];
+    return LogDetailPageController(game);
+  }
+
+}
+
+class GamePageBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<LogDetailPageController>(() => LogDetailPageController.withGetX());
+  }
+}
